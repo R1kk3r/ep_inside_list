94df20e251c0895d533ffedf59502c767f22936d
R1kk3r <gael.jobin@switzerlandmail.ch>
2015-05-23 18:03:31 +0200

    Fix width problem that allow writing below the chat/user section

--- hooks.js
+++ hooks.js
@@ -12,7 +12,7 @@ exports.eejsBlock_body = function (hook_name, args, cb) {
 
     // Resize central pad to allow a left panel
     var index = args.content.indexOf("editorcontainerbox");
-    args.content = args.content.slice(0,index+19) + "style = \"left:182px;\" " + args.content.slice(index+19);
+    args.content = args.content.slice(0,index+19) + "style = \"width:auto;left:182px;right:0px;\" " + args.content.slice(index+19);
 
     // Add left panel containing pads list
     args.content = args.content + eejs.require("ep_inside_list/templates/padsListHTML.ejs", {pads : result});
